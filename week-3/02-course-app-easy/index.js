81da0a46d2ba2a8d270bd1aa9e75155da46113d8
Om Jaiswal <omjaiswal2552@gmail.com>
2023-07-20 14:33:14 +0530

    easy coding

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,25 +7,57 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+const adminAuthentication = (req, res, next) => {
+  const {username, password} = req.headers;
+  const admin = ADMINS.find(a => a.username === username && a.password === password)
+  if (admin) {
+    next();
+  } else {
+    res.status(403).json({message: "Admin authentication failed"})
+  }
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const admin = req.body
+  const existingAdmin = ADMINS.find(a => a.username === admin.username)
+  if (existingAdmin){
+    res.status(403).json({message: 'Admin already exist'})
+  } else {
+    USERS.push(admin)
+    res.status(200).json({message: "Admin created sussessfully"})
+  }
 });
 
-app.post('/admin/login', (req, res) => {
+app.post('/admin/login', adminAuthentication, (req, res) => {
   // logic to log in admin
+  res.status(200).json({message: "Logged in sussessfully"})
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', adminAuthentication, (req, res) => {
   // logic to create a course
+  const course = req.body;
+  course.id = Date.now()
+  COURSES.push(course)
+  res.json({message: "Course created successfully", courseId: course.id})
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put('/admin/courses/:courseId',adminAuthentication, (req, res) => {
   // logic to edit a course
+  const courseId = parseInt(req.params.courseId)
+  const course = COURSES.find(c => c.id === courseId);
+  if (course){
+    Object.assign(course, req.body);
+    res.json({message: "Course updated sussessfully"})
+  } else {
+    res.status(404).json({message: "Course not found"})
+  }
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get('/admin/courses', adminAuthentication, (req, res) => {
   // logic to get all courses
+  res.json({courses: COURSES});
 });
 
 // User routes
